f9d35ed8ed5d2127474a17752c4862b07cfbdd4a
Alex <al.lyzhin@gmail.com>
2022-01-03 00:20:37 +0300

    add eslint expception for debounce function

--- src/utils.ts
+++ src/utils.ts
@@ -4,12 +4,12 @@ export const isEscKeyDown = (evt: KeyboardEvent) => evt.keyCode === KeyCode.ESC;
 export const isEnterKeyDown = (evt: KeyboardEvent) => evt.keyCode === KeyCode.ENTER;
 
 export const debounce = (
-  callback: (...params: any[]) => any,
+  callback: (...params: any[]) => any, // eslint-disable-line
   delay: number,
 ) => {
   let timeoutId: ReturnType<typeof setTimeout>;
 
-  return (...args: any[]) => {
+  return (...args: any[]) => { // eslint-disable-line
     clearTimeout(timeoutId);
     timeoutId = setTimeout(() => callback.apply(this, args), delay);
   };
